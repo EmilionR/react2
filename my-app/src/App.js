6f5f075b365204d056a8374cf811b07270f1b8d5
Emilion <emiljadersten@gmail.com>
2024-02-28 19:17:21 +0100

    Snälla

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
+      <NavBarSimple />
       <Sidebar />
+      <StatefulGreeting greeting="I love Mew!" />
     </div>
   );
 }
---
+++ my-app/src/components/StatefulGreeting.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Babygirl!",
+            buttonText: "Kiss",
+            count: 0
+        };
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            console.log("Previous state", prevState);
+            console.log("Previous props", prevProps);
+            return {
+                introduction: prevState.introduction === "Hii noi!" ? "Babygirl!" : "Hii noi!",
+                buttonText: prevState.buttonText === "Fuck" ? "Kiss" : "Fuck"
+            }
+        });
+    }
+
+    increment() {
+        this.setState((prevState, prevProps) => {
+            console.log("Previous state", prevState);
+            console.log("Previous props", prevProps);
+            return {
+                count: prevState.count + 1
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.increment()}>{this.state.count}</button>
+            </div>
+        )
+    }
+}
+
+export default StatefulGreeting;
